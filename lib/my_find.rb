dd6e3e407865ced813f6ac4b0cca26b3c3a33629
David Zeit <dzeit216@gmail.com>
2018-08-30 21:11:53 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
 def my_find(collection)
-  i = 0 
+  i = 0
   while i < collection.length
     yield(collection[i])
       return collection[1]
-      # or return collection[1] if yield(collection[1])
-    end 
-    i += 0 
-  end 
+      # or return collection[1] if yield(collection[1]) with no "end" on the next line
+    end
+    i += 0
+  end
 end
